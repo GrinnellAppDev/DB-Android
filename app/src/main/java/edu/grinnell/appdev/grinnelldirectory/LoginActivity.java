4197ea749c4de2c9a1f2325f40677c1984ad5252
as-f <as-f@users.noreply.github.com>
2017-02-12 14:12:10 -0600

    add onclick handler to signin button

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/LoginActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/LoginActivity.java
@@ -2,12 +2,37 @@ package edu.grinnell.appdev.grinnelldirectory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.username) EditText username;
+    @BindView(R.id.password) EditText password;
+    @BindView(R.id.signin) Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.signin)
+    public void signin(View view) {
+        String usernameTxt = username.getText().toString();
+        String passwordTxt = password.getText().toString();
+
+        // toast username for testing
+        Toast.makeText(this, usernameTxt, Toast.LENGTH_SHORT).show();
+        // TODO send username & password to API
     }
 }
