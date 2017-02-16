45fe9253573d952797a0331c00db50e5c691acad
nicholasroberson <roberson@grinnell.edu>
2017-02-15 21:34:35 -0600

    reformatted MainActivity.class

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/MainActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/MainActivity.java
@@ -1,13 +1,15 @@
 package edu.grinnell.appdev.grinnelldirectory.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.io.IOException;
 import java.util.List;
 
 import edu.grinnell.appdev.grinnelldirectory.Interfaces.DatabaseAPI;
 import edu.grinnell.appdev.grinnelldirectory.Model.Person;
+import edu.grinnell.appdev.grinnelldirectory.R;
 import edu.grinnell.appdev.grinnelldirectory.User;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,44 +17,50 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import edu.grinnell.appdev.grinnelldirectory.R;
-
 public class MainActivity extends AppCompatActivity {
 
-    public String baseUrl = "https://itwebappstest.grinnell.edu/DotNet/WebServices/api/";
-    public Retrofit retrofit;
-    public  DatabaseAPI dbAPI;
+    private String baseUrl = "https://itwebappstest.grinnell.edu/DotNet/WebServices/api/";
+    private Retrofit retrofit;
+    private DatabaseAPI dbAPI;
+    private Call<List<Person>> personQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setup();
+    }
 
+    public void setup() {
         retrofit = new Retrofit.Builder().
                 baseUrl(baseUrl).
                 addConverterFactory(GsonConverterFactory.create()).
                 build();
 
-        Log.e("STARTED_MAIN","MAIN ACTIVITY STARTED");
-        User user = new User("test1stu","selfserv1");
+        dbAPI = retrofit.create(DatabaseAPI.class);
+
+        User user = new User("test1stu", "selfserv1");
         makeAPICall(user);
     }
 
     public void makeAPICall(User user) {
-        constructCall();
+        personQuery = dbAPI.getStudents(user, "Nicholas", "Roberson", "", "", "", "", "", "", "", "", "", "", "", "");
 
-        Call<List<Person>> personQuery = dbAPI.getStudents(user,"Prabir","Pradhan","","");
         personQuery.enqueue(new Callback<List<Person>>() {
+
             @Override
             public void onResponse(Call<List<Person>> call, Response<List<Person>> response) {
-                //Log.e("RESPONSE",response.body().getClassYear());
                 if (response.body() != null) {
                     Log.d("API_SUCCESS", "API returned list of people.");
                     // response.body() is the list of people, set to 'people'
                     List<Person> people = response.body();
-                    Log.e("TEST",people.get(0).getClassYear().toString());
+                    Log.e("TEST", people.get(0).getClassYear().toString());
                 } else {
-                    Log.e("API_FAILURE", "onResult() called but fields were null");
+                    try {
+                        Log.e("BAD_CALL", response.errorBody().string());
+                    } catch (IOException e) {
+                        Log.e("API_FAILURE_EXCEPTION", e.toString());
+                    }
                 }
             }
 
@@ -62,9 +70,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    public void constructCall() {
-        dbAPI = retrofit.create(DatabaseAPI.class);
     }
 }
