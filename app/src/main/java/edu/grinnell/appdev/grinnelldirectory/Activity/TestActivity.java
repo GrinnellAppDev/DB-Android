e226fdd184aae91bf74cd6541f87d9edd999b18c
nicholasroberson <roberson@grinnell.edu>
2017-02-22 08:46:32 -0600

    Fixed issues w/ merge

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/TestActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/TestActivity.java
@@ -7,10 +7,11 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.grinnell.appdev.grinnelldirectory.APICaller;
 import edu.grinnell.appdev.grinnelldirectory.Interfaces.APICallerInterface;
 import edu.grinnell.appdev.grinnelldirectory.Model.Person;
 import edu.grinnell.appdev.grinnelldirectory.R;
-import edu.grinnell.appdev.grinnelldirectory.User;
+import edu.grinnell.appdev.grinnelldirectory.Model.User;
 
 /**
  * Created by nicholasroberson on 2/22/17.
