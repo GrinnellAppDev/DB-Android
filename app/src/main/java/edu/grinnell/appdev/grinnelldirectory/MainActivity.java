2d892a569de1de782a87596466e42ce19eb51cae
as-f <as-f@users.noreply.github.com>
2017-02-22 22:24:24 -0600

    remove test from main activity

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/MainActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.grinnell.appdev.grinnelldirectory;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,10 +8,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // switch to login activity to test it
-        Intent login = new Intent(this, LoginActivity.class);
-        startActivity(login);
     }
 }
