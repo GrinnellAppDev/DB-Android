49fb952ed0315c2c7ef7b5581e9fd38c7ecf5a89
as-f <as-f@users.noreply.github.com>
2017-02-24 17:19:06 -0600

    move simple search to its own activity

---
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/SimpleSearchActivity.java
@@ -0,0 +1,14 @@
+package edu.grinnell.appdev.grinnelldirectory.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.grinnell.appdev.grinnelldirectory.R;
+
+public class SimpleSearchActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_simple_search);
+  }
+}
