522628b1a43643b4051371eed1b51a2ce98dbf55
mudabek <nazarovo@grinnell.edu>
2017-02-26 13:56:56 -0600

    manifest

---
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/AdvancedSearch.java
@@ -0,0 +1,17 @@
+package edu.grinnell.appdev.grinnelldirectory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Otabek Nazarov on 2/19/2017.
+ */
+
+public class AdvancedSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.advanced_search);
+    }
+}
