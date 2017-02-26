4300d334af91d35f6360e93d2af29634d28fab01
as-f <as-f@users.noreply.github.com>
2017-02-26 13:11:46 -0600

    add onclick event to search button

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/SimpleSearchActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/SimpleSearchActivity.java
@@ -3,12 +3,26 @@ package edu.grinnell.appdev.grinnelldirectory.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import edu.grinnell.appdev.grinnelldirectory.R;
 
 public class SimpleSearchActivity extends AppCompatActivity {
+  @BindView(R.id.query) private EditText mQueryEditText;
+  @BindView(R.id.search) private Button mSearchButton;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_simple_search);
+    ButterKnife.bind(this);
+  }
+
+  @OnClick(R.id.search)
+  private void seach(View view) {
+    String queryText = mQueryEditText.getText().toString();
   }
 }
