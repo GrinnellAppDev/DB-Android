a91841d0e883437e71dce3c7da9e6ba9ff429d9c
Ritika Agarwal <riitka.dabest@gmail.com>
2017-03-01 22:33:32 -0600

    completed the Search Results Recycler view adapter class, used picasso forthe image of the person

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/SearchResultsAdapter.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/SearchResultsAdapter.java
@@ -1,11 +1,19 @@
 package edu.grinnell.appdev.grinnelldirectory;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import edu.grinnell.appdev.grinnelldirectory.Model.Person;
 
 /**
  * Created by ritikaagarwal on 2/26/17.
@@ -14,20 +22,52 @@ import android.widget.TextView;
 public class SearchResultsAdapter extends
         RecyclerView.Adapter<SearchResultsAdapter.ViewHolder> {
 
+    // Store a member variable for the Persons list
+    private List<Person> mPersons;
+    // Store the context for easy access
+    private Context mContext;
 
-    @Override
-    public SearchResultsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    // Pass in the contact array into the constructor
+    public SearchResultsAdapter(Context context, List<Person> persons) {
+        mPersons = persons;
+        mContext = context;
+    }
+    private Context getContext() {
+        return mContext;
     }
 
     @Override
-    public void onBindViewHolder(SearchResultsAdapter.ViewHolder holder, int position) {
+    public SearchResultsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
+        // Inflate the custom layout
+        View personView = inflater.inflate(R.layout.item_searchresult, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(personView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(SearchResultsAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Person person = mPersons.get(position);
+
+        // Set item views based on your views and data model
+        ImageView imageView = viewHolder.personImage;
+        Picasso.with(mContext).load(person.getImgPath()).into(imageView);
+        TextView textView1 = viewHolder.name;
+        textView1.setText(person.getFirstName() + " " + person.getLastName());
+        TextView textView2 = viewHolder.major;
+        textView2.setText(person.getMajor());
+        TextView textView3 = viewHolder.location;
+        textView3.setText(person.getAddress());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mPersons.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
