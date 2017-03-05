a4b18f57d3c732b66022627c0cfed71007586865
as-f <as-f@users.noreply.github.com>
2017-03-05 14:21:38 -0600

    move APICallerInterface methods to LoginActivity.java

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/LoginActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/LoginActivity.java
@@ -23,7 +23,7 @@ import java.util.List;
  */
 
 // make this activity default?
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements APICallerInterface {
     @BindView(R.id.username)
     EditText mUsernameEditText;
     @BindView(R.id.password)
@@ -45,45 +45,56 @@ public class LoginActivity extends AppCompatActivity {
      */
     @OnClick(R.id.login)
     void signIn(View view) {
-        final String username = mUsernameEditText.getText().toString();
-        final String password = mPasswordEditText.getText().toString();
+        String username = mUsernameEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
         User user = new User(username, password);
-        DBAPICaller dbapiCaller = new DBAPICaller(user, new APICallerInterface() {
-            @Override public void onSearchSuccess(List<Person> people) {
-                // never supposed to get called
-            }
-
-            /**
-             * Save credentials and move to MainActivity if login succeeded
-             * @param success whether the login was successful
-             * @param person model for the logged in user
-             */
-            @Override public void authenticateUserCallSuccess(boolean success, Person person) {
-                if (success) {
-                    User.saveCredentials(getApplicationContext(), username, password);
-                    User.saveUserDetails(getApplicationContext(), person);
-
-                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-                    builder.setMessage(R.string.authentication_failure);
-                    builder.show();
-                }
-            }
-
-            @Override public void onServerFailure(String fail_message) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-                builder.setMessage(R.string.server_failure + fail_message);
-                builder.show();
-            }
-
-            @Override public void onNetworkingError(String fail_message) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-                builder.setMessage(R.string.networking_error + fail_message);
-                builder.show();
-            }
-        });
+        DBAPICaller dbapiCaller = new DBAPICaller(user, this);
         dbapiCaller.authenticateUser();
+    }
+
+    @Override public void onSearchSuccess(List<Person> people) {}
+
+    /**
+     * Save credentials and move to MainActivity if login succeeded
+     *
+     * @param success whether the login was successful
+     * @param person model for the logged in user
+     */
+    @Override public void authenticateUserCallSuccess(boolean success, Person person) {
+        String username = mUsernameEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+        if (success) {
+            User.saveCredentials(getApplicationContext(), username, password);
+            User.saveUserDetails(getApplicationContext(), person);
+
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            startActivity(intent);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+            builder.setMessage(R.string.authentication_failure);
+            builder.show();
+        }
+    }
+
+    /**
+     * Show an error message if the server returns an error
+     *
+     * @param fail_message error description
+     */
+    @Override public void onServerFailure(String fail_message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setMessage(R.string.server_failure + fail_message);
+        builder.show();
+    }
+
+    /**
+     * Show an error message if the network has an error
+     *
+     * @param fail_message error description
+     */
+    @Override public void onNetworkingError(String fail_message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setMessage(R.string.networking_error + fail_message);
+        builder.show();
     }
 }
