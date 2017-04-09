4415148b86c15fab2ad4f1242fe79497b5a67354
as-f <as-f@users.noreply.github.com>
2017-04-09 13:49:01 -0500

    implement simple search

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/SimpleSearchActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/SimpleSearchActivity.java
@@ -1,24 +1,38 @@
 package edu.grinnell.appdev.grinnelldirectory.Activity;
 
+import android.content.Intent;
+import android.os.Parcel;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.google.gson.Gson;
+import edu.grinnell.appdev.grinnelldirectory.DBAPICaller;
+import edu.grinnell.appdev.grinnelldirectory.Interfaces.APICallerInterface;
+import edu.grinnell.appdev.grinnelldirectory.Model.Person;
+import edu.grinnell.appdev.grinnelldirectory.Model.SimpleResult;
+import edu.grinnell.appdev.grinnelldirectory.Model.User;
 import edu.grinnell.appdev.grinnelldirectory.R;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * SimpleSearchActivity allows the user to search with one text field.
  */
 
-public class SimpleSearchActivity extends AppCompatActivity {
+public class SimpleSearchActivity extends AppCompatActivity implements APICallerInterface {
     @BindView(R.id.first_name_field) EditText mFirstNameEditText;
     @BindView(R.id.last_name_field) EditText mLastNameEditText;
     @BindView(R.id.search) Button mSearchButton;
+
+    public static final String BUNDLE_KEY = "BUNDLE_KEY";
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +45,63 @@ public class SimpleSearchActivity extends AppCompatActivity {
      *
      * @param view SimpleSearchActvity's view
      */
-    @OnClick(R.id.search)
-    void search(View view) {
+    @OnClick(R.id.search) void search(View view) {
+        User user = User.getUser(this);
+
         String firstName = mFirstNameEditText.getText().toString();
         String lastName = mLastNameEditText.getText().toString();
+
+        DBAPICaller dbapiCaller = new DBAPICaller(user, this);
+
+        List<String> query = new ArrayList<>();
+        query.add(firstName);
+        query.add(lastName);
+        query.add("");
+        query.add("");
+
+        dbapiCaller.simpleSearch(query);
+    }
+
+    /**
+     * Bundle people and move to SearchResults Activity if search successful
+     *
+     * @param people List of person models
+     */
+    @Override public void onSearchSuccess(List<Person> people) {
+        Intent intent = new Intent(this, MainActivity.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putParcelable(BUNDLE_KEY, new SimpleResult(people));
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    @Override public void authenticateUserCallSuccess(boolean success, Person person) {
+    }
+
+    /**
+     * Show an error message if the server returns an error
+     *
+     * @param fail_message error description
+     */
+    @BindString(R.string.server_failure) String serverFailure;
+    @Override public void onServerFailure(String fail_message) {
+        showAlert(serverFailure + fail_message);
+    }
+
+    /**
+     * Show an error message if the network has an error
+     *
+     * @param fail_message error description
+     */
+    @BindString(R.string.networking_error) String networkingError;
+    @Override public void onNetworkingError(String fail_message) {
+        showAlert(networkingError + fail_message);
+    }
+
+    private void showAlert(String message) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.show();
     }
 }
---
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Model/SimpleResult.java
@@ -0,0 +1,36 @@
+package edu.grinnell.appdev.grinnelldirectory.Model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import java.util.List;
+
+public class SimpleResult implements Parcelable {
+
+    List<Person> people;
+
+    static Parcelable.Creator CREATOR = new Parcelable.Creator<SimpleResult>() {
+        @Override public SimpleResult createFromParcel(Parcel parcel) {
+            return new SimpleResult(parcel);
+        }
+
+        public SimpleResult[] newArray(int size) {
+            return new SimpleResult[size];
+        }
+    };
+
+    public SimpleResult(List<Person> people) {
+        this.people = people;
+    }
+
+    public SimpleResult(Parcel parcel) {
+        people = (List<Person>) parcel.readValue(SimpleResult.class.getClassLoader());
+    }
+
+    @Override public int describeContents() {
+        return 0;
+    }
+
+    @Override public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeValue(people);
+    }
+}
