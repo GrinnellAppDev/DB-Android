a6bdfd3042f081ab9cab2eeea9d8b75bf9821838
mudabek <nazarovo@grinnell.edu>
2017-04-12 21:11:50 -0500

    added hint

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/MainActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/Activity/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.grinnell.appdev.grinnelldirectory.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import edu.grinnell.appdev.grinnelldirectory.R;
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Intent intent = new Intent(this, AdvancedSearchActivity.class);
+        startActivity(intent);
+    }
 }
