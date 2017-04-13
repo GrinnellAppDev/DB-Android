f3943bf41b3f55aaec388b4d21982ad779b5e01d
as-f <as-f@users.noreply.github.com>
2017-04-12 21:25:14 -0500

    create test for parceling

---
+++ app/src/androidTest/java/edu/grinnell/appdev/grinnelldirectory/models/SimpleResultTest.java
@@ -0,0 +1,36 @@
+package edu.grinnell.appdev.grinnelldirectory.models;
+
+import android.os.Bundle;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class SimpleResultTest {
+
+    @Test
+    public void unparceledParcelEqualityTest() {
+        List<Person> people = new ArrayList();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(SimpleResult.SIMPLE_KEY, new SimpleResult(people));
+
+        SimpleResult result = bundle.getParcelable(SimpleResult.SIMPLE_KEY);
+        assertNotNull("SimpleResult should not be null", result);
+
+        List<Person> newPeople = result.getPeople();
+
+        people.equals(newPeople); // this won't work because Person is an object
+        assertEquals("The SimpleResults should have the same length", people.size(), newPeople.size());
+
+        for (int i = 0; i < people.size(); i++) {
+            Gson gson = new Gson();
+            String peopleJson = gson.toJson(people);
+            String newPeopleJson = gson.toJson(newPeople);
+            assert("Each person should equal each new person", peopleJson.equals(newPeopleJson);
+        }
+    }
+
+}
--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/models/SimpleResult.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/models/SimpleResult.java
@@ -6,31 +6,65 @@ import java.util.List;
 
 public class SimpleResult implements Parcelable {
 
-    List<Person> mPeople;
+    public static final String SIMPLE_KEY = "SIMPLE_KEY";
 
-    static Parcelable.Creator CREATOR = new Parcelable.Creator<SimpleResult>() {
+    private List<Person> mPeople;
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator<SimpleResult>() {
+        /**
+         * Create a new SimpleResult by deserializing parcel
+         * @param parcel A parcel created by SimpleResult.writeToParcel
+         * @return a deserialiezd SimpleResult object
+         */
         @Override public SimpleResult createFromParcel(Parcel parcel) {
             return new SimpleResult(parcel);
         }
 
-        public SimpleResult[] newArray(int size) {
+        /**
+         * Create a new array of type SimpleResult[] with everything initialized to null
+         * @param size the size of the array to be created
+         * @return a new array of type SimpleResult[] with everything initialized to null
+         */
+        @Override public SimpleResult[] newArray(int size) {
             return new SimpleResult[size];
         }
     };
 
+    /**
+     * Create a new SimpleResult with non-serialized data
+     * @param people A list of people
+     */
     public SimpleResult(List<Person> people) {
         this.mPeople = people;
     }
 
+    public List<Person> getPeople() {
+        return mPeople;
+    }
+
+    /**
+     * Create a new SimpleResult by deserializing parcel
+     * @param parcel A parcel created by SimpleResult.writeToParcel
+     * @return a deserialiezd SimpleResult object
+     */
     public SimpleResult(Parcel parcel) {
         mPeople = (List<Person>) parcel.readValue(SimpleResult.class.getClassLoader());
     }
 
+    /**
+     * @return a bitmask that specifies if parcelable includes a file descriptor
+     */
     @Override public int describeContents() {
         return 0;
     }
 
-    @Override public void writeToParcel(Parcel parcel, int i) {
+    /**
+     * serialize `this` into `parcel`
+     * @oaram parcel The parcel that the SimpleResult gets serialized into
+     * @param flags Defaults to 0, equals Parcelable.PARCELABLE_WRITE_RETURN_VALUE if object being
+     * written is a return value
+     */
+    @Override public void writeToParcel(Parcel parcel, int flags) {
         parcel.writeValue(mPeople);
     }
 }
