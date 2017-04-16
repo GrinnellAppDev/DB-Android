05cb9bf16da231293d9c1bc88499496be450f143
as-f <as-f@users.noreply.github.com>
2017-04-16 14:30:58 -0500

    change to 4 space indentation

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/adapters/SearchResultsAdapter.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/adapters/SearchResultsAdapter.java
@@ -13,64 +13,63 @@ import edu.grinnell.appdev.grinnelldirectory.R;
 
 import java.util.List;
 
-
 public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.ViewHolder> {
 
-  private List<Person> mPersons;
-  private Context mContext;
+    private List<Person> mPersons;
+    private Context mContext;
 
-
-  public SearchResultsAdapter(Context context, List<Person> persons) {
-      mContext = context;
-  }
-
-  public void updateData(List<Person> persons) {
-      mPersons = persons;
-      notifyDataSetChanged();
-  }
-
-  @Override
-  public SearchResultsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    final LayoutInflater inflater = LayoutInflater.from(mContext);
-    View personView = inflater.inflate(R.layout.item_searchresult, parent, false);
-    return new ViewHolder(personView);
-  }
-
-  @Override public void onBindViewHolder(SearchResultsAdapter.ViewHolder viewHolder, int position) {
-    Person person = mPersons.get(position);
-
-    ImageView personImageView = viewHolder.personImage;
-    TextView nameTextView = viewHolder.name;
-    TextView majorTextView = viewHolder.major;
-    TextView locationTextView = viewHolder.location;
-
-    Picasso.with(mContext).load(person.getImgPath()).into(personImageView);
-    nameTextView.setText(person.getFirstName() + " " + person.getLastName());
-    majorTextView.setText(person.getMajor());
-    locationTextView.setText(person.getAddress());
-  }
-
-  @Override public int getItemCount() {
-    return mPersons == null ? 0 : mPersons.size();
-  }
-
-  /**
-   * ViewHolder class to hold the views from the inflated layout for each list item
-   */
-  public static class ViewHolder extends RecyclerView.ViewHolder {
-    public ImageView personImage;
-    public TextView name;
-    public TextView major;
-    public TextView location;
-
-    public ViewHolder(View itemView) {
-
-      super(itemView);
-
-      name = (TextView) itemView.findViewById(R.id.tv_name);
-      major = (TextView) itemView.findViewById(R.id.tv_major);
-      location = (TextView) itemView.findViewById(R.id.tv_location);
-      personImage = (ImageView) itemView.findViewById(R.id.iv_personImage);
+    public SearchResultsAdapter(Context context, List<Person> persons) {
+        mContext = context;
     }
-  }
+
+    public void updateData(List<Person> persons) {
+        mPersons = persons;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public SearchResultsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final LayoutInflater inflater = LayoutInflater.from(mContext);
+        View personView = inflater.inflate(R.layout.item_searchresult, parent, false);
+        return new ViewHolder(personView);
+    }
+
+    @Override
+    public void onBindViewHolder(SearchResultsAdapter.ViewHolder viewHolder, int position) {
+        Person person = mPersons.get(position);
+
+        ImageView personImageView = viewHolder.personImage;
+        TextView nameTextView = viewHolder.name;
+        TextView majorTextView = viewHolder.major;
+        TextView locationTextView = viewHolder.location;
+
+        Picasso.with(mContext).load(person.getImgPath()).into(personImageView);
+        nameTextView.setText(person.getFirstName() + " " + person.getLastName());
+        majorTextView.setText(person.getMajor());
+        locationTextView.setText(person.getAddress());
+    }
+
+    @Override public int getItemCount() {
+        return mPersons == null ? 0 : mPersons.size();
+    }
+
+    /**
+     * ViewHolder class to hold the views from the inflated layout for each list item
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView personImage;
+        public TextView name;
+        public TextView major;
+        public TextView location;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.tv_name);
+            major = (TextView) itemView.findViewById(R.id.tv_major);
+            location = (TextView) itemView.findViewById(R.id.tv_location);
+            personImage = (ImageView) itemView.findViewById(R.id.iv_personImage);
+        }
+    }
 }
