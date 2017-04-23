7acf722e55ffca6c6b8efd7ec18f61dc645027c9
nicholasroberson <roberson@grinnell.edu>
2017-04-23 13:35:19 -0500

    Added colors and designed elements for the main activity (searchPagerActivity)

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/activities/SearchPagerActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/activities/SearchPagerActivity.java
@@ -1,10 +1,15 @@
 package edu.grinnell.appdev.grinnelldirectory.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.io.Serializable;
@@ -23,4 +28,32 @@ public class SearchPagerActivity extends AppCompatActivity implements Serializab
         ViewPager searchPager = (ViewPager) findViewById(R.id.pager);
         searchPager.setAdapter(new SearchPagerAdapter(getSupportFragmentManager()));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            // send user to the login screen
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_about) {
+            // pop up a dialog fragment that has a description of the app and how to use it.
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
