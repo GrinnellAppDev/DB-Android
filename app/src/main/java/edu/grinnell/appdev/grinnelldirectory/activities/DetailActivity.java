ffdb40490d0d2e62ee4c3d24ec4e0eff28783bca
Prabir Pradhan <prabirmsp@gmail.com>
2017-05-06 22:04:40 -0500

    zoomable image view in person detail page

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/activities/DetailActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/activities/DetailActivity.java
@@ -1,9 +1,15 @@
 package edu.grinnell.appdev.grinnelldirectory.activities;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,14 +17,29 @@ import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import edu.grinnell.appdev.grinnelldirectory.R;
 import edu.grinnell.appdev.grinnelldirectory.models.Person;
 
 public class DetailActivity extends AppCompatActivity {
 
+    public static final int ANIMATION_DURATION = 500;
+
     Person p;
+    boolean isImageZoomed;
+    Pair<Float, Float> screenDimens;
+    Pair<Float, Float> initialPicDimens;
+    Pair<Float, Float> zoomedPicTranslate;
+
+    @BindView(R.id.relative_layout)
+    View relativeLayout;
     @BindView(R.id.pic)
     ImageView pic;
+    @BindView(R.id.card_view)
+    CardView picHolder;
+    float picScaleFactor;
+    @BindView(R.id.dimmer)
+    View dimmer;
     @BindView(R.id.name)
     TextView name;
     @BindView(R.id.year)
@@ -141,4 +162,98 @@ public class DetailActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    public void onBackPressed() {
+        if (isImageZoomed) {
+            unZoomImage();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @OnClick(R.id.pic)
+    public void onClickImage() {
+        if (isImageZoomed)
+            unZoomImage();
+        else
+            zoomImage();
+    }
+
+    @OnClick(R.id.dimmer)
+    public void onClickDimmer() {
+        if (isImageZoomed)
+            unZoomImage();
+    }
+
+    void zoomImage() {
+        calculateDimens();
+        animateTranslation(0, 0, zoomedPicTranslate.first, zoomedPicTranslate.second, picHolder);
+        animateScale(1f, picScaleFactor, picHolder);
+        animateAlpha(0f, 1f, dimmer);
+        dimmer.setVisibility(View.VISIBLE);
+        isImageZoomed = true;
+    }
+
+    void unZoomImage() {
+        animateTranslation(zoomedPicTranslate.first, zoomedPicTranslate.second, 0, 0, picHolder);
+        animateScale(picScaleFactor, 1f, picHolder);
+        animateAlpha(1f, 0f, dimmer);
+        dimmer.setVisibility(View.GONE);
+        isImageZoomed = false;
+    }
+
+    void animateAlpha(float from, float to, View target) {
+        Animation animDarken = new AlphaAnimation(from, to);
+        animDarken.setFillAfter(true);
+        animDarken.setDuration(ANIMATION_DURATION);
+        animDarken.setInterpolator(new OvershootInterpolator());
+        target.startAnimation(animDarken);
+    }
+
+    void animateScale(float from, float to, View target) {
+        ObjectAnimator animScaleNewX = ObjectAnimator.ofFloat(target, "scaleX", from, to);
+        ObjectAnimator animScaleNewY = ObjectAnimator.ofFloat(target, "scaleY", from, to);
+        animScaleNewX.setDuration(ANIMATION_DURATION);
+        animScaleNewY.setDuration(ANIMATION_DURATION);
+        animScaleNewX.setInterpolator(new OvershootInterpolator());
+        animScaleNewY.setInterpolator(new OvershootInterpolator());
+        animScaleNewX.start();
+        animScaleNewY.start();
+    }
+
+    void animateTranslation(float fromWidth, float fromHeight, float toWidth, float toHeight, View target) {
+        ObjectAnimator animTransNewX = ObjectAnimator.ofFloat(target, "translationX", fromWidth, toWidth);
+        ObjectAnimator animTransNewY = ObjectAnimator.ofFloat(target, "translationY", fromHeight, toHeight);
+        animTransNewX.setDuration(ANIMATION_DURATION);
+        animTransNewY.setDuration(ANIMATION_DURATION);
+        animTransNewX.setInterpolator(new OvershootInterpolator());
+        animTransNewY.setInterpolator(new OvershootInterpolator());
+        animTransNewX.start();
+        animTransNewY.start();
+    }
+
+    /**
+     * Calculates the dimensions and coordinates used for zooming the person
+     * image when clicked.
+     */
+    private void calculateDimens() {
+        // dimensions of the view container
+        float screenWidth = relativeLayout.getWidth();
+        float screenHeight = relativeLayout.getHeight();
+        screenDimens = new Pair<>(screenWidth, screenHeight);
+
+        // dimensions of the picture in the beginning
+        float picWidth = (float) pic.getWidth();
+        float picHeight = (float) pic.getHeight();
+        initialPicDimens = new Pair<>(picWidth, picHeight);
+
+        picScaleFactor = Math.min((0.7f * screenHeight) / picHeight, (0.7f * screenWidth) / picWidth);
+
+        // distance to translate the view when
+        float picZoomX = screenWidth / 2f - (picHolder.getWidth() / 2f + picHolder.getLeft());
+        float picZoomY = screenHeight / 2f - (picHolder.getHeight() / 2f + picHolder.getTop());
+        zoomedPicTranslate = new Pair<>(picZoomX, picZoomY);
+    }
+
 }
