f9420cbf14b269880ccf472faee3143a39c36888
Prabir Pradhan <prabirmsp@gmail.com>
2017-05-11 15:50:19 -0500

    add laucher icon and add fabric

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/DBApplication.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/DBApplication.java
@@ -1,12 +1,15 @@
 package edu.grinnell.appdev.grinnelldirectory;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class DBApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         if (!EncryptionUtils.isEncryptionKeySet(this)) {
             EncryptionUtils.setAppEncryptionKey(this, "" + System.currentTimeMillis());
