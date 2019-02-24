ea9744cb37ebe3a98b95dce8bed2a3d9fb847183
as-f <as-f@users.noreply.github.com>
2019-02-24 13:04:45 -0600

    Remove unused code and fix email verification logic error

--- app/src/main/java/edu/grinnell/appdev/grinnelldirectory/activities/DetailActivity.java
+++ app/src/main/java/edu/grinnell/appdev/grinnelldirectory/activities/DetailActivity.java
@@ -1,15 +1,12 @@
 package edu.grinnell.appdev.grinnelldirectory.activities;
 
-import android.Manifest;
 import android.animation.ObjectAnimator;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -317,7 +314,7 @@ public class DetailActivity extends AppCompatActivity {
         /* get username */
         String uname = (String) username.getText();
 
-        if (uname != null || !uname.isEmpty()) {
+        if (uname != null && !uname.isEmpty()) {
             /* send mail and handle exception if no mail app is found */
             try {
                 String email = getString(R.string.base_email, uname.substring(1, uname.length() - 1));
@@ -349,30 +346,6 @@ public class DetailActivity extends AppCompatActivity {
 
             } catch (ActivityNotFoundException e) {
                 e.printStackTrace();
-            }
-        }
-    }
-
-    /**
-     * Function to open call app on phone with an active call to the person the user is viewing.
-     */
-    private void sendCall() {
-        Intent callIntent = new Intent(Intent.ACTION_CALL); //use ACTION_CALL class
-        callIntent.setData(Uri.parse(getString(R.string.phone_uri, person.getPhone())));    //this is the phone number calling
-        //check permission
-        //If the device is running Android 6.0 (API level 23) and the app's targetSdkVersion is 23 or higher,
-        //the system asks the user to grant approval.
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            //request permission from user if the app hasn't got the required permission
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CALL_PHONE},   //request specific permission from user
-                    10);
-        } else {     //have got permission
-            try {
-                startActivity(callIntent);  //call activity and make phone call
-            }
-            catch (android.content.ActivityNotFoundException ex){
-                Toast.makeText(getApplicationContext(),"Error: could not place call.",Toast.LENGTH_SHORT).show();
             }
         }
     }
